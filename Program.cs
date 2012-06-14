b589b168779da884ae20923adb2ca7cfb07cd556
mwudka <mwudka@me.com>
2012-06-14 15:25:21 -0700

    Created VS2010 project

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Amazon;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+using Amazon.SimpleDB;
+using Amazon.SimpleDB.Model;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace windows_s3_uploader
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      
+    }
+
+  }
+}
